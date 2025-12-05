e99c4fdd24374d8bc5ea93bedfac816ea5df7513
vncezxc <deresvince@gmail.com>
2025-12-05 15:14:41 +0800

    template

--- cvsu_internship/settings.py
+++ cvsu_internship/settings.py
@@ -38,6 +38,22 @@ DATABASES = {
 
 ROOT_URLCONF = 'cvsu_internship.urls'
 
+# Templates
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
