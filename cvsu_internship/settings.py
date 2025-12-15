7322301402aeac3969821f955d8946304163388d
vncezxc <deresvince@gmail.com>
2025-12-15 13:59:09 +0800

    Add custom storage backend to fix bucket11

--- cvsu_internship/settings.py
+++ cvsu_internship/settings.py
@@ -359,6 +359,8 @@ AWS_QUERYSTRING_AUTH = False
 AWS_S3_FILE_OVERWRITE = False
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 
+AWS_S3_ADDRESSING_STYLE = 'virtual'
+
 # Cloudinary Configuration
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': get_config('CLOUDINARY_CLOUD_NAME', default=''),
